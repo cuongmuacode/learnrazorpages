9156bdac7aaf31491d28cf2584e64ca39ba88f05
Lưu Mạnh Cường <cuonglmsinx@gmail.com>
2025-03-19 04:24:58 +0700

    add model

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using LearnRazorPages.Data;
 namespace LearnRazorPages
 {
     public class Program
@@ -5,6 +8,8 @@ namespace LearnRazorPages
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddRazorPages();
